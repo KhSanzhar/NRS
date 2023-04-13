f2524441f7ad73a873c1fc6c2c41f4390523b11d
KhSanzhar <sanzhkhalel@gmail.com>
2023-04-13 23:53:01 +0600

    top-bar

--- front_end/NRS/src/app/app-routing.module.ts
+++ front_end/NRS/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent} from "./components/home/home.component";
+import { MyRecipesComponent } from "./components/my-recipes/my-recipes.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
+import { RecipesComponent } from "./components/recipes/recipes.component";
+import { SignUpComponent } from "./components/sign-up/sign-up.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'my-recipes', component: MyRecipesComponent},
+  {path: 'recipes', component: RecipesComponent},
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'sign-up', component: SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front_end/NRS/src/app/app.module.ts
+++ front_end/NRS/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { MyRecipesComponent } from './components/my-recipes/my-recipes.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    HomeComponent,
+    MyRecipesComponent,
+    RecipesComponent,
+    AboutUsComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,

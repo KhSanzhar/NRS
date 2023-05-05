4f6d16bed56525ce3fedf7d165b5468ee6ba2bf1
pp221B030731 <n_cherniyazov@kbtu.kz>
2023-05-05 14:35:16 +0600

    nothing changed(

--- back_end/our_nrs/our_nrs/settings.py
+++ back_end/our_nrs/our_nrs/settings.py
@@ -152,7 +152,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:4200',
+)
